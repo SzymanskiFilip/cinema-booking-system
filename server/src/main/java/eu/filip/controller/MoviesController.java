a62966f7961f37298ef2c2e7fa2ab15f6eb63ffa
Filip <szymanski.fp@gmail.com>
2022-08-08 23:11:08 +0200

    finished retrieving pages of movies

--- server/src/main/java/eu/filip/controller/MoviesController.java
+++ server/src/main/java/eu/filip/controller/MoviesController.java
@@ -2,17 +2,20 @@ package eu.filip.controller;
 
 import eu.filip.service.MovieService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ApiController {
+public class MoviesController {
     private MovieService movieService;
 
-    public ApiController(MovieService movieService) {
+    public MoviesController(MovieService movieService) {
         this.movieService = movieService;
     }
 
-    public ResponseEntity<?> getMovies(){
-        return new ResponseEntity.ok(movieService.findPaginated(1, 10));
+    @GetMapping("/movies")
+    public ResponseEntity<?> getMovies(@RequestParam(name = "pageNumber") int pageNumber, @RequestParam(name = "pageSize") int pageSize){
+        return ResponseEntity.ok(movieService.findPaginated(pageNumber, pageSize));
     }
 }
--- server/src/main/java/eu/filip/service/MovieService.java
+++ server/src/main/java/eu/filip/service/MovieService.java
@@ -1,2 +1,26 @@
-package eu.filip.service;public class MovieService {
+package eu.filip.service;
+
+import eu.filip.entity.Movie;
+import eu.filip.repository.MovieRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+    private MovieRepository movieRepository;
+
+    public MovieService(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+
+    public List<Movie> findPaginated(int pageNo, int pageSize){
+        Pageable paging = PageRequest.of(pageNo, pageSize);
+        Page<Movie> pagedResult = movieRepository.findAll(paging);
+
+        return pagedResult.toList();
+    }
 }
