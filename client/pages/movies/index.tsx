77e1d18e191ad48077db06071c746cfc12f2f437
Filip <szymanski.fp@gmail.com>
2022-08-08 23:22:22 +0200

    fetching movies on load

--- client/pages/movies/index.tsx
+++ client/pages/movies/index.tsx
@@ -1,6 +1,21 @@
 import {NextPage} from "next";
 
-const Movies: NextPage = () => {
+type movie = {
+    id: number;
+    title: string;
+    year_of_release: number;
+    duration: number;
+    cover_url: string;
+}
+
+type PageProps = {
+    movies: movie[];
+}
+
+const Movies: NextPage<PageProps> = ({movies}) => {
+
+    console.log(movies)
+
     return(
         <div>
             movies
@@ -9,3 +24,15 @@ const Movies: NextPage = () => {
 }
 
 export default Movies;
+
+export async function getServerSideProps(){
+
+    const res = await fetch("http://localhost:8080/movies?pageNumber=0&pageSize=10");
+    const data = await res.json();
+
+    return {
+        props: {
+            movies: data
+        }
+    }
+}
