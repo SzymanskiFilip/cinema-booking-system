533490116715d52081f112d54156b1fdcc91e1b2
Filip <szymanski.fp@gmail.com>
2022-08-08 19:53:30 +0200

    updated schema

--- server/src/main/resources/schema.sql
+++ server/src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 create table movies (
-    id bigint primary key,
+    id bigint primary key auto_increment,
     title varchar(60) not null,
     year_of_release int not null,
     duration int not null,
-    cover_url text
+    cover_url text not null
 );
 
 create table seance (
-    id bigint primary key ,
+    id bigint primary key auto_increment,
     movie_id bigint not null ,
     play_date datetime not null ,
     available_seats int not null default 50,
@@ -15,7 +15,7 @@ create table seance (
 );
 
 create table seats (
-    id bigint primary key ,
+    id bigint primary key auto_increment,
     seance_id bigint primary key,
     seat_number int not null ,
     email varchar(320) not null ,
