3b8a0311310c0abc2dedcd751788aab6bfd32d45
Filip <szymanski.fp@gmail.com>
2022-08-13 00:20:26 +0200

    getting images from backend and changed width to 200px

--- client/pages/components/movies/SeanceRow.tsx
+++ client/pages/components/movies/SeanceRow.tsx
@@ -20,14 +20,14 @@ function SeanceRow({data}: seance): JSX.Element{
 
     return(
         <div className="movie-row">
-            <img alt="123" src="https://img.actvid.com/xxrz/250x400/189/ea/b4/eab48fa4c71804c00008616715dd3777/eab48fa4c71804c00008616715dd3777.jpg"/>
+            <img alt="123" src={data.movie.coverURL}/>
             <div className="movie-row__content">
                 <h1 className="text-5xl">{data.movie.title}</h1>
                 <p>Date: {getDateString(date)}</p>
                 <div className="flex flex-row">
                     <div className="movie-row__content__block">
                         <h1>DURATION</h1>
-                        <h1>{data.movie.duration}</h1>
+                        <h1>{data.movie.duration}min</h1>
                     </div>
                     <div className="movie-row__content__block">
                         <h1>YEAR OF RELEASE</h1>
