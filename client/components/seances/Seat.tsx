aee2386766b2f954f1c47538194f73e725171fa7
Filip <szymanski.fp@gmail.com>
2022-08-25 20:44:15 +0200

    finished selecting seats

--- client/components/seances/Seat.tsx
+++ client/components/seances/Seat.tsx
@@ -1,14 +1,30 @@
+import {useState} from "react";
+import seatsList from "./SeatsList";
+
 type props = {
     id: number;
     taken: boolean;
 }
-
 function Seat({id, taken}: props): JSX.Element{
 
+    const [selected, setSelected] = useState<boolean>(false);
 
+    function getStyle(): string{
+        let style: string = "seat ";
+        if(taken){
+            style += "seat__taken";
+            return style;
+        } else {
+            style += "seat__free";
+            return style;
+        }
+    }
 
     return(
-        <div className={taken ? "seat seat__taken" : "seat seat__free"}>
+        <div
+            className={selected ? "seat seat__selected" : getStyle()}
+            onClick={() => setSelected(!selected)}
+        >
             <h1>{id}</h1>
         </div>
     )
